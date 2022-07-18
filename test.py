089279241c69c1bcceafec18456c600e16ed48f3
sominik37 <somi_nik@yahoo.com>
2022-07-18 20:01:17 +0500

    comment added

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import random
 import string
 
+#passgen dunction
+
 def PassGen(length):
     ran = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(length))
     print(ran)
