850b8a580a29daa37fa5c3178cf73dedc2f11103
usman <somi_nik@yahoo.com>
2022-07-18 19:26:03 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def PassGen(length):
+    ran = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(length))
+    print(ran)
+length = random.randint(8, 15)
+PassGen(length)
