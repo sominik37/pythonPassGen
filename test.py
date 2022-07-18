8b8c8f6ebf77d91fdcefd9b0bac2fa69d2bb7567
sominik37 <somi_nik@yahoo.com>
2022-07-18 20:09:22 +0500

    2nd

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import random
 import string
 
-#passgen dunction
+#passgen function
 
 def PassGen(length):
     ran = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(length))
     print(ran)
-length = random.randint(8, 15)
+length = random.randint(10, 15)
 PassGen(length)
